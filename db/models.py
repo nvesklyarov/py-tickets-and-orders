816fc63f21df565e54bc75324c4853e58597f045
Nazariy Vesklyarov <nazariy.vesklyarov@gmail.com>
2025-08-15 13:56:47 +0300

    SolutionNV2

--- db/models.py
+++ db/models.py
@@ -57,37 +57,41 @@ class MovieSession(models.Model):
 
 
 class Order(models.Model):
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey("User", on_delete=models.CASCADE)
 
     class Meta:
         ordering = ["-created_at"]
 
     def __str__(self) -> str:
-        return f"{self.created_at}"
+        return f"<Order: {self.created_at}>"
 
 
 class Ticket(models.Model):
-    movie_session = models.ForeignKey(MovieSession, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    movie_session = models.ForeignKey(MovieSession, on_delete=models.CASCADE, related_name="tickets")
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets")
     row = models.IntegerField()
     seat = models.IntegerField()
 
     def __str__(self) -> str:
-        return (f"{self.movie_session.movie.title} "
-                f"{self.movie_session.show_time} "
-                f"(row: {self.row}, seat: {self.seat})")
+        return (
+            f"<Ticket: {self.movie_session.movie.title} "
+            f"{self.movie_session.show_time} "
+            f"(row: {self.row}, seat: {self.seat})>"
+        )
 
-    def clean(self) -> bool:
-        if self.row > self.movie_session.cinema_hall.rows:
+    def clean(self) -> None:
+        rows = self.movie_session.cinema_hall.rows
+        seats_in_row = self.movie_session.cinema_hall.seats_in_row
+
+        if self.row < 1 or self.row > rows:
             raise ValidationError({
-                "row": ["row number must be in available "
-                        "range: (1, rows): (1, 10)"]
+                "row": [f"row number must be in available range: 1-{rows}"]
             })
-        if self.seat > self.movie_session.cinema_hall.seats_in_row:
+
+        if self.seat < 1 or self.seat > seats_in_row:
             raise ValidationError({
-                "seat": ["seat number must be in available "
-                         "range: (1, seats_in_row): (1, 12)"]
+                "seat": [f"seat number must be in available range: 1-{seats_in_row}"]
             })
 
     def save(self, *args, **kwargs) -> None:
